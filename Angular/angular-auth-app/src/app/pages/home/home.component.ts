0a32dba55e786ae7605213da05dc13663563b993
biplobpustcse <biplobpustcse@gmail.com>
2025-05-20 18:39:15 +0600

    photo

--- Angular/angular-auth-app/src/app/pages/home/home.component.ts
+++ Angular/angular-auth-app/src/app/pages/home/home.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { HousingService } from '../../services/housing.service';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor(private auth: AuthService) {}
+  housingLocationList: any[] = [];
+  filteredLocationList: any[] = [];
+  constructor(private auth: AuthService,private housingService: HousingService) {
+    this.housingLocationList = housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+   filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((location) =>
+      location.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 
   logout() {
     this.auth.logout();
